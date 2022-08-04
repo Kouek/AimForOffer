1d3e409e0dfc7da0a2394c7921d84997009278d7
Kouek <598601889@qq.com>
2022-08-05 00:45:17 +0800

    replace_space
    	modified:   edition_1/CMakeLists.txt
    	new file:   edition_1/build_tree.cpp
    	modified:   edition_1/find_number_in_2D_array.cpp
    	new file:   edition_1/replace_space.cpp

--- edition_1/find_number_in_2D_array.cpp
+++ edition_1/find_number_in_2D_array.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-class Solution {
+class SolutionNaive {
   public:
     bool findNumberIn2DArray(vector<vector<int>> &matrix, int target) {
         int rowNum = matrix.size();
@@ -34,6 +34,8 @@ class Solution {
             for (int col = colS; col <= colE; ++col)
                 if (matrix[row][col] == target)
                     return true;
+                else if (matrix[row][col] > target)
+                    break;
         return false;
     }
 };
@@ -41,7 +43,24 @@ class Solution {
 class SolutionBST {
   public:
     bool findNumberIn2DArray(vector<vector<int>> &matrix, int target) {
-        // TODO
+        if (matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        std::array<size_t, 2> curr{matrix.size() - 1, 0};
+        int colNum = matrix[0].size();
+        while (true) {
+            if (curr[1] == colNum)
+                return false;
+            auto val = matrix[curr[0]][curr[1]];
+            if (val == target)
+                return true;
+            else if (val > target)
+                if (curr[0] == 0)
+                    return false;
+                else
+                    curr[0] -= 1;
+            else
+                curr[1] += 1;
+        }
     }
 };
 
@@ -51,6 +70,8 @@ int main() {
                             {3, 6, 9, 16, 22},
                             {10, 13, 14, 17, 24},
                             {18, 21, 23, 26, 30}};
+    //mat = {{-1,3}};
+    //SolutionNaive solution;
     SolutionBST solution;
-    cout << solution.findNumberIn2DArray(mat, 32);
+    cout << solution.findNumberIn2DArray(mat, 3);
 }
